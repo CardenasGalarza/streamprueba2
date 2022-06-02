3e43ec28c98dc938573a857f0ce69b9ebc583370
CardenasGalarza <alex10estadistica@gmail.com>
2022-06-02 17:24:47 -0500

    hg7g6g

--- app.py
+++ app.py
@@ -1,60 +1,71 @@
-import streamlit as st  # pip install streamlit
-import pandas as pd  # pip install pandas
-import plotly.express as px  # pip install plotly-express
-import base64  # Standard Python Module
-from io import StringIO, BytesIO  # Standard Python Module
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from PIL import Image
 
+st.set_page_config(page_title='Survey Results')
+st.header('Survey Results 2021')
+st.subheader('Was the tutorial helpful?')
 
-def generate_excel_download_link(df):
-    # Credit Excel: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474/5
-    towrite = BytesIO()
-    df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
-    towrite.seek(0)  # reset pointer
-    b64 = base64.b64encode(towrite.read()).decode()
-    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
-    return st.markdown(href, unsafe_allow_html=True)
+### --- LOAD DATAFRAME
+excel_file = 'Survey_Results.xlsx'
+sheet_name = 'DATA'
 
-def generate_html_download_link(fig):
-    # Credit Plotly: https://discuss.streamlit.io/t/download-plotly-plot-as-html/4426/2
-    towrite = StringIO()
-    fig.write_html(towrite, include_plotlyjs="cdn")
-    towrite = BytesIO(towrite.getvalue().encode())
-    b64 = base64.b64encode(towrite.read()).decode()
-    href = f'<a href="data:text/html;charset=utf-8;base64, {b64}" download="plot.html">Download Plot</a>'
-    return st.markdown(href, unsafe_allow_html=True)
+df = pd.read_excel(excel_file,
+                   sheet_name=sheet_name,
+                   usecols='B:D',
+                   header=3)
 
+df_participants = pd.read_excel(excel_file,
+                                sheet_name= sheet_name,
+                                usecols='F:G',
+                                header=3)
+df_participants.dropna(inplace=True)
 
-st.set_page_config(page_title='Excel Plotter')
-st.title('Excel Plotter 📈')
-st.subheader('Feed me with your Excel file')
+# --- STREAMLIT SELECTION
+department = df['Department'].unique().tolist()
+ages = df['Age'].unique().tolist()
 
-uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
-if uploaded_file:
-    st.markdown('---')
-    df = pd.read_excel(uploaded_file, engine='openpyxl')
-    st.dataframe(df)
-    groupby_column = st.selectbox(
-        'What would you like to analyse?',
-        ('Ship Mode', 'Segment', 'Category', 'Sub-Category'),
-    )
+age_selection = st.slider('Age:',
+                        min_value= min(ages),
+                        max_value= max(ages),
+                        value=(min(ages),max(ages)))
 
-    # -- GROUP DATAFRAME
-    output_columns = ['Sales', 'Profit']
-    df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
+department_selection = st.multiselect('Department:',
+                                    department,
+                                    default=department)
 
-    # -- PLOT DATAFRAME
-    fig = px.bar(
-        df_grouped,
-        x=groupby_column,
-        y='Sales',
-        color='Profit',
-        color_continuous_scale=['red', 'yellow', 'green'],
-        template='plotly_white',
-        title=f'<b>Sales & Profit by {groupby_column}</b>'
-    )
-    st.plotly_chart(fig)
+# --- FILTER DATAFRAME BASED ON SELECTION
+mask = (df['Age'].between(*age_selection)) & (df['Department'].isin(department_selection))
+number_of_result = df[mask].shape[0]
+st.markdown(f'*Available Results: {number_of_result}*')
 
-    # -- DOWNLOAD SECTION
-    st.subheader('Downloads:')
-    generate_excel_download_link(df_grouped)
-    generate_html_download_link(fig)
+# --- GROUP DATAFRAME AFTER SELECTION
+df_grouped = df[mask].groupby(by=['Rating']).count()[['Age']]
+df_grouped = df_grouped.rename(columns={'Age': 'Votes'})
+df_grouped = df_grouped.reset_index()
+
+# --- PLOT BAR CHART
+bar_chart = px.bar(df_grouped,
+                   x='Rating',
+                   y='Votes',
+                   text='Votes',
+                   color_discrete_sequence = ['#F63366']*len(df_grouped),
+                   template= 'plotly_white')
+st.plotly_chart(bar_chart)
+
+# --- DISPLAY IMAGE & DATAFRAME
+col1, col2 = st.beta_columns(2)
+image = Image.open('images/survey.jpg')
+col1.image(image,
+        caption='Designed by slidesgo / Freepik',
+        use_column_width=True)
+col2.dataframe(df[mask])
+
+# --- PLOT PIE CHART
+pie_chart = px.pie(df_participants,
+                title='Total No. of Participants',
+                values='Participants',
+                names='Departments')
+
+st.plotly_chart(pie_chart)
