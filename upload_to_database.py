de30077247f8d26202fa04afb590efbe9bef673f
CardenasGalarza <alex10estadistica@gmail.com>
2022-06-22 09:08:15 -0500

    sfefr

--- upload_to_database.py
+++ upload_to_database.py
@@ -2,9 +2,9 @@ import streamlit_authenticator as stauth
 
 import database as db
 
-usernames = ["LLLERENAL", "rmiller","Cardenas", "mgarciab", "jbravob"]
-names = ["Luis Llerena Lagunes", "Rebecca Miller", 'Giancarlos Cardenas', "Mauro Arturo Garcia", "John Jairo Bravo"]
-passwords = ["Smnz$1304$La", "def456", "cardenas10", "Gaddiel$14", "48557917"]
+usernames = ["LLLERENAL", "rmiller","Cardenas", "mgarciab", "jbravob", "amedinav"]
+names = ["Luis Llerena Lagunes", "Rebecca Miller", 'Giancarlos Cardenas', "Mauro Arturo Garcia", "John Jairo Bravo", "Alfredo"]
+passwords = ["Smnz$1304$La", "def456", "cardenas10", "Gaddiel$14", "48557917", "Gaddiel$14"]
 hashed_passwords = stauth.Hasher(passwords).generate()
 
 
