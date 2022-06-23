fa83f74abee13e20f3c101b9aea0c0ae284fce07
CardenasGalarza <alex10estadistica@gmail.com>
2022-06-23 04:39:49 -0500

    jnnnnhn

--- setup copy.sh
+++ setup copy.sh
@@ -6,14 +6,6 @@ email = \"alex10estadistica@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
-[theme]\n\
-base='light'\n\
-primaryColor='#3a70ec'\n\
-backgroundColor='#4359de'\n\
-secondaryBackgroundColor = '#F0F2F6'\n\
-textColor='#0e1862'\n\
-font = 'sans serif'\n\
-
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
