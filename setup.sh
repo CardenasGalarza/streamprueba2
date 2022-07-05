a9ab9d4c5c627757dd60705bdc63579dacd1a61a
CardenasGalarza <alex10estadistica@gmail.com>
2022-07-04 20:12:50 -0500

    eded

--- setup.sh
+++ setup.sh
@@ -8,10 +8,10 @@ email = \"alex10estadistica@gmail.com\"\n\
 echo "\
 [theme]\n\
 base='light'\n\
-primaryColor='#3a70ec'\n\
-backgroundColor='#4359de'\n\
-secondaryBackgroundColor = '#F0F2F6'\n\
-textColor='#0e1862'\n\
+primaryColor='#f63366'\n\
+backgroundColor='#355c7d'\n\
+secondaryBackgroundColor = '#d6d5d5'\n\
+textColor='#000000'\n\
 font = 'sans serif'\n\
 
 [server]\n\
