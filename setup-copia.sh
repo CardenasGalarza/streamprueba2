0dc8d42b3117a0ab82d7f1d96c8772689561b3ab
CardenasGalarza <alex10estadistica@gmail.com>
2022-07-07 09:33:37 -0500

    eded

--- setup-copia.sh
+++ setup-copia.sh
@@ -6,14 +6,6 @@ email = \"alex10estadistica@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
-[theme]\n\
-base='light'\n\
-primaryColor='#f63366'\n\
-backgroundColor='#355c7d'\n\
-secondaryBackgroundColor = '#d6d5d5'\n\
-textColor='#000000'\n\
-font = 'sans serif'\n\
-
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
